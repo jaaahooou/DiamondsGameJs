d1c870f3947624d3de9ca0da10a699ec2f8ba060
Jacek Pilch <jaaahooou@wp.pl>
2021-08-19 19:16:03 +0200

    add

--- scripts/MainMenu.esm.js
+++ scripts/MainMenu.esm.js
@@ -1,5 +1,13 @@
-import { Common } from "./Common.esm.js";
+import { Common, HIDDEN_SCREEN, VISIBLE_SCREEN } from "./Common.esm.js";
 
-class MainMenu {}
+const START_SCREEN_GAME_BUTTON_ID = "js-start-game";
+const START_SCREEN_ID = "js-start-screen";
+const START_SCREEN_SETTINGS_BUTTON_ID = "js-settings-button";
+
+class MainMenu extends Common {
+  constructor() {
+    super();
+  }
+}
 
 export const mainMenu = new MainMenu();
